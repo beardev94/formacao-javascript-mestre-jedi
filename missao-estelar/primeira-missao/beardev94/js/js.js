8434bdea4aaf09bf8148a0b73368ae1462f25326
beardev <beardev94@gmail.com>
2020-03-10 16:06:23 -0300

    Missão Estelar JS - Realizada com sucesso!!!:)

---
+++ missao-estelar/primeira-missao/beardev94/js/js.js
@@ -0,0 +1,12 @@
+//declaração de variáveis
+var missao = "Missão Estelar JS em execução!!!";
+
+//declaração de funções
+function eventoOnload(){
+    console.log("Evento onload disparado");
+}
+
+function eventoOnclick(){
+    console.log("Missão estelar JS realçizada com sucesso!!!");
+}
+
